1b381711efee445155d7bb9eb83d482ad3f21933
Andrey <andreykulakov619@gmail.com>
2024-07-03 19:38:06 +0300

    Lesson_5

--- lesson/Main.java
+++ lesson/Main.java
@@ -1,7 +1,23 @@
 package lesson;
 
+import static lesson.Lesson_5.*;
+
 public class Main {
     public static void main(String[] args) {
 
-    }
+    printThreeWords(); /*1 задание*/
+    checkSumSign(); /*2 задание*/
+    printColor(); /*3 задание*/
+    compareNumbers(); /*4 задание*/
+    System.out.println(isSumInRange()); /*5 задание*/
+    printPositiveOrNegative(); /*6 задание*/
+    System.out.println(isNegative()); /*7 задание*/
+    printStringMultipleTimes(); /*8 задание*/
+    System.out.println(isLeapYear()); /*9 задание*/
+    invertBinaryArray(); /*10 задание*/
+    fillArray(); /*11 задание*/
+    multiplyElements(); /*12 задание*/
+    fillDiagonal(); /*13 задание*/
+    createArray(); /*14 задание*/
+}
 }
