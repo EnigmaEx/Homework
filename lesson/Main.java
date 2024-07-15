9a50684c8bacbe4baf7e716c01c0968d6255c9f6
Andrey <andreykulakov619@gmail.com>
2024-07-15 18:12:28 +0300

    Lesson_5

--- lesson/Main.java
+++ lesson/Main.java
@@ -1,23 +1,34 @@
-package lesson;
+package Lesson_5;
 
-import static lesson.Lesson_5.*;
+import static Lesson_5.Lesson_5.*;
 
 public class Main {
     public static void main(String[] args) {
+        printThreeWords(); /*1 задание*/
+        checkSumSign(-8, 6); /*2 задание*/
+        printColor(89); /*3 задание*/
+        compareNumbers(13, 9); /*4 задание*/
+        System.out.println(isSumInRange(3, 17)); /*5 задание*/
+        printPositiveOrNegative(-3); /*6 задание*/
+        System.out.println(isNegative(-13)); /*7 задание*/
+        printStringMultipleTimes("Aston", 2); /*8 задание*/
+        System.out.println(isLeapYear(1985)); /*9 задание*/
 
-    printThreeWords(); /*1 задание*/
-    checkSumSign(); /*2 задание*/
-    printColor(); /*3 задание*/
-    compareNumbers(); /*4 задание*/
-    System.out.println(isSumInRange()); /*5 задание*/
-    printPositiveOrNegative(); /*6 задание*/
-    System.out.println(isNegative()); /*7 задание*/
-    printStringMultipleTimes(); /*8 задание*/
-    System.out.println(isLeapYear()); /*9 задание*/
-    invertBinaryArray(); /*10 задание*/
-    fillArray(); /*11 задание*/
-    multiplyElements(); /*12 задание*/
-    fillDiagonal(); /*13 задание*/
-    createArray(); /*14 задание*/
-}
+        int[] binaryArray = {1, 0, 1, 0, 1, 1, 1}; /*10 задание*/
+        invertBinaryArray(binaryArray);
+        printArray(binaryArray);
+
+        int[] hundredArray = fillArray(100); /*11 задание*/
+        printArray(hundredArray);
+
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}; /*12 задание*/
+        multiplyElements(array);
+        printArray(array);
+
+        int[][] matrix = fillDiagonal(3); /*13 задание*/
+        printMatrix(matrix);
+
+        int[] filledArray = createArray(8, 2); /*14 задание*/
+        printArray(filledArray);
+    }
 }
