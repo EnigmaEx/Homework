11753cc0695395447d88c916bfa56d727e8aead4
Andrey <andreykulakov619@gmail.com>
2024-07-15 18:16:19 +0300

    Lesson_5

--- lesson/Main.java
+++ lesson/Main.java
@@ -3,6 +3,7 @@ package Lesson_5;
 import static Lesson_5.Lesson_5.*;
 
 public class Main {
+
     public static void main(String[] args) {
         printThreeWords(); /*1 задание*/
         checkSumSign(-8, 6); /*2 задание*/
