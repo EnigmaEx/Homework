37d3d3c5604a2f0b905cc4c59f5629d44ee98cfe
Andrey <andreykulakov619@gmail.com>
2024-08-02 01:01:33 +0300

    commit

---
+++ factorial/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        int number = 5;
+        System.out.printf("Factorial %d = %d%n", number, factorial.getFactorial(number));
+    }
+}
